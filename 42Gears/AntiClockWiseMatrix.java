ad8e0b0c79f202b9a525bc365d016776d997d3b0
Rakesh <dr8452799@gmail.com>
2025-07-25 14:37:13 +0530

    Update AntiClockWiseMatrix.java

--- 42Gears/AntiClockWiseMatrix.java
+++ 42Gears/AntiClockWiseMatrix.java
@@ -13,6 +13,7 @@ public class AnticlockWiseMatrix {
 	public static void main(String[] args) {
 		int [] [] matrix = {{1,2,3,4},
 				{5,6,7,8}};
+		// Calls Static Method RoatateMatrix and store result in reversedMatrix
 		int [][] reversedMatrix = RotateMatrix(matrix);
 		System.out.println("roatated Array");
 		for(int rows []:reversedMatrix)
@@ -20,15 +21,21 @@ public class AnticlockWiseMatrix {
 			System.out.println(Arrays.toString(rows));
 		}
 	}
+	// Static Method of RotateMatrix
 	static int [][] RotateMatrix(int matrix [][])
 	{
+		// Length of rows and colunmns
 		int rows = matrix.length;
 		int cols = matrix[0].length;
+		// Create a new matrix of size cols x rows → 4 x 2 to store the rotated result.
 		int [] [] reversedMatrix = new int [cols][rows];
 		for(int i=0;i<rows;i++)
 		{
 			for(int j=0;j<cols;j++)
 			{
+				//reversedMatrix[4 - 0 - 1][0] = matrix[0][0];
+                                //reversedMatrix[3][0] = 1;
+
 				reversedMatrix[cols-j-1][i] = matrix[i][j];
 			}
 		}
