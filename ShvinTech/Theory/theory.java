306679adfca1fa081d4b869e2eb89671c3cdbcc0
Rakesh <dr8452799@gmail.com>
2025-11-06 06:10:10 +0530

    Add OOP questions and coding task to theory.java
    Added questions and coding task related to OOP concepts and Java.

--- ShvinTech/Theory/theory.java
+++ ShvinTech/Theory/theory.java
@@ -1,1 +1,11 @@
-hi
+Explain About oops?
+Diff b/w final, finally?
+Diff b/w Array, ArrayList?
+Diff b/w == , .equals()?
+Diff blw Sets, Maps?
+Diff b/w Comparator & Comparable?
+Which Set Doesn't Store Duplicate, Heterogenous Data?
+How do You Handle Exceptions?
+
+<------Coding--------->
+write a program to print 1 to 100 without using loops?
