6995df505f992bce1ef342e9e1b3a66b4b625969
Rakesh <dr8452799@gmail.com>
2025-11-12 06:20:48 +0530

    Update and rename Theory.java to TechnicalRound.java

---
+++ Baryons Software/TechnicalRound.java
@@ -0,0 +1,12 @@
+Explain About OOPS?
+what is Sql?
+About Sping Boot?
+what is status codes 200,201,400?
+what is Spring Actuators?
+what are Datatypes in Spring?
+What is Has-a-Relationship?
+About Java 8 Features?
+About Lamda Expression?
+Write any Sql Query?
+Write a Program to check Palindrome(String) or Not?
+  
