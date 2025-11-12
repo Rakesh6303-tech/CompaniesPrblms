07b6e8946a5900a021a84b756f332175dfd6fbec
Rakesh <dr8452799@gmail.com>
2025-11-12 06:15:37 +0530

    Add coding round tasks for interview process
    Added coding round tasks for interview preparation.

--- Baryons Software/Coding.java
+++ Baryons Software/Coding.java
@@ -1,1 +1,12 @@
+Baryons Solutions Hiring Pattern
+  1) Resume Shortlisting
+  2) Online Aptitude
+  3) Offline Coding Round
+  4) Technical Round
+  5) CEO Round
 
+<-------------Coding Round Offline------------------->
+  1) Write a program to print factorial of a given number 5 for 120?
+  2) write a program to print Reverse Word of String "Hello World" to "olleh dlrow"?
+  3) Write a program to print 2nd highest number in array/list [12,45,22,66,78]?
+  4) create a class name called Car and assign attributes like CarBrand "BMW", Year "2018" and create object of that class and print attributes?
