2a4b610e01b9ca4e706df9e342fa6acff4e39912
Rakesh <dr8452799@gmail.com>
2025-11-12 06:31:15 +0530

    Create CEORound.java

---
+++ Baryons Software/CEORound.java
@@ -0,0 +1,13 @@
+Self Intro?
+Code Snippet wrote in Paper? We have to explain..
+You Graduated in 2024 since from what you did?
+Ur Basically From CSE right,  Why did you took Training Course?
+What is Computer?
+What is "new" keyword in java?
+What is Construtor in java?
+How many Codes you wrote in College?<100
+You Don't have passion, interest, to get job?
+How can i Assure You?
+How many Interviews you were Attended till yet?(She)
+
+
