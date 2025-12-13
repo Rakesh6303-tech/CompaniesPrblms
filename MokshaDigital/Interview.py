f4b2f32987cf01fcae1a568ab82f570916c50b8d
Rakesh <dr8452799@gmail.com>
2025-12-13 22:24:18 +0530

    Create Interview.py

---
+++ MokshaDigital/Interview.py
@@ -0,0 +1,9 @@
+<----------------HR Round----------------->
+About Self?
+AI is there why should i Hire you?
+what do u do in ur free time?
+
+<-------------------Technical Round----------->
+What is Time Complexity & Space Complexity?
+Write a Bubble sort Algorithm?Explain?
+Explain about online food delivery project?
