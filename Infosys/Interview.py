ee499b453e98e1e1e5c8b97aec3e027b6cba17e1
Rakesh <dr8452799@gmail.com>
2025-12-13 22:15:15 +0530

    Create Interview.py

---
+++ Infosys/Interview.py
@@ -0,0 +1,5 @@
+Explain about the Final Year Project?
+Explain about Random Forest Algorithm Indetail  including steps?
+what is mean by CRUD?How many ways you can perform CRUD operations?
+What is Servlets?
+Which one is better like Core Java or Advance Java?
