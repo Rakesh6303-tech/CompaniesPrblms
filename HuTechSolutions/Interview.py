619921d9340c5e0201d9c89b0b2214b6313883d9
Rakesh <dr8452799@gmail.com>
2025-12-13 22:10:27 +0530

    Add interview questions to Interview.py

---
+++ HuTechSolutions/Interview.py
@@ -0,0 +1,14 @@
+<--------------Coding Round------------------->
+What is Immutability?Give Examples.How can u create Immutable Class?
+Given a sentence, find the 2nd highest (second longest) word and print the word along with its length using Java Streams?
+
+
+
+<----------Interview Questions-------->
+What are the Java 8 Features?
+write a query to print city of  average ordersamount from the table?
+TableName : Orders, Columns : order_id, city, order_amount, Date.
+Project Releated : Your API code looks good and doesn’t need changes. Currently, the API response time is around 300ms.
+If we want to reduce it to 200ms or even 100ms, what changes would you make specifically in the SQL query?
+
+
