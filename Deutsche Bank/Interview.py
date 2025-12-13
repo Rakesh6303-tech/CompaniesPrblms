2d0275ec847ac612932694f3c48eb00d0ad7c2f0
Rakesh <dr8452799@gmail.com>
2025-12-13 22:21:05 +0530

    Create Interview.py

---
+++ Deutsche Bank/Interview.py
@@ -0,0 +1,17 @@
+<--------------Technical Round---------------->
+Explain about ur EMS Project?
+How did u connect front end to Backend?
+Authentication is required why don't u included?
+Explain about OOPS concepts?
+Write Example for Encapsulation?
+Difference Btw Span and Div Tag?
+Create a Btw and when i click it will goes to another page and image should be appeared in that next page?
+Explain about MultiThreading?
+
+<------------------HR Round-------------->
+Self Intro?
+What do you gain from the L1 Interview?
+What is ur biggest challenge that u faced?
+what is ur strength & weakness?
+she asked me some Documents like addhar,pan,marksmemo,passport etc?
+  
